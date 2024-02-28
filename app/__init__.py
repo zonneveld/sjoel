91b9e6a40ce27dcb4cd823449526a87f337529c9
zonneveld <zdj2606@novacollege.nl>
2024-02-28 12:56:26 +0100

    basic functionality

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from app import routes
+# from app import machine
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 
-from flask import render_template
+from flask import render_template, Response,request
 
 from app import app
+
+from app.machine import Machine
+
+import time
+
 
 @app.route('/')
 @app.route('/index')
@@ -13,21 +18,51 @@ def index():
 def aanmelden():
     return render_template('aanmelden.html')
 
-@app.route('/speel')
+@app.route('/speel',methods = ['GET'])
 def speel():
-    testspelers =[   
-        {
-            'naam':'jantje',
-            'score':0
-        },
+    # print(request.args.get("speler1"))
+    # print(request.args.)
+    huidgespelers = []
+    id = 0    
+    for data in request.args:
+
+        if request.args.get(data) != "":
+            huidgespelers.append(
+                {
+                    'naam':request.args.get(data),
+                    'id':id
+                }
+            )
+            id += 1
+    # testspelers =[   
+    #     {
+    #         'naam':'jantje',
+    #         'score':0
+    #     },
         
-        {
-            'naam':'pietje',
-            'score':0
-        }
-    ]
-    return render_template('speel.html', spelers = testspelers)
+    #     {
+    #         'naam':'pietje',
+    #         'score':0
+    #     }
+    # ]
+    return render_template('speel.html', spelers = huidgespelers)
 
 @app.route('/winnaar')
 def winnaar():
     return render_template('winnaar.html')
+
+@app.route('/listen')
+def stream_data():
+    def generator():
+        machine = Machine()
+        machine.start()
+        while True:
+            while not machine.check():
+                pass
+            r_data = machine.get_payload()
+            machine.flush()
+            data = f'data: {r_data}\n\n'
+            print(f"yielding {r_data}")
+            yield data
+            
+    return Response(generator(),mimetype='text/event-stream')
